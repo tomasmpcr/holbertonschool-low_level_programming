ecd4b38d772acd9a909ccaa83474b1e84d0038c5
tshuna332 <tmmi333@gmail.com>
2021-02-04 17:08:11 +0000

    auto add

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i == '9' && j == '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
