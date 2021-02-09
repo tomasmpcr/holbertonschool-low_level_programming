e989b41a229b4b60427f57ce151554ccc63456ce
Tshuna332 <tmmi333@gmail.cm>
2021-02-09 12:37:21 +0000

    auto add

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,12 @@
 #include "holberton.h"
 #include "1-alphabet.c"
 
-void print_alphabet_x10()
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
 {
 	char i;
 
