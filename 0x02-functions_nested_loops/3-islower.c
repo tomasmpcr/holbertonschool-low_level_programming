4e8bd6af0d67a70ec427a41c90c13d4e6ac69802
Tshuna332 <tmmi333@gmail.cm>
2021-02-09 12:47:20 +0000

    auto add

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,19 @@
 #include "holberton.h"
 
+/**
+ * _islower - return islower
+ * @c: number the char
+ *
+ * Return: Always 0 (Success)
+ */
 int _islower(int c)
 {
-	_putchar('\n');
-	_putchar(c);
-	_putchar('\n');
-	_putchar('\n');
-	return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
 }
