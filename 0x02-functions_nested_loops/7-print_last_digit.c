ff75a43c828133f0536b7d921694324133b6f1df
Tshuna332 <tmmi333@gmail.cm>
2021-02-09 13:27:54 +0000

    auto add

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - return print_last_digit
+ * @n: number
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
