70f71091c199ce652a223965f6c24170c56ac690
Tshuna332 <tmmi333@gmail.cm>
2021-02-09 15:52:51 +0000

    auto add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - return add
+ *@n1: n1
+ *@n2: n2
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int n1, int n2)
+{
+	int r;
+
+	r = n1 + n2;
+	return (r);
+}
