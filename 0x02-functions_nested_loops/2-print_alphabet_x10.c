65a3ea85cd8d641a464617d025af5a3d44eb66b5
Tshuna332 <tmmi333@gmail.cm>
2021-02-09 12:21:35 +0000

    auto add

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include "1-alphabet.c"
+
+void print_alphabet_x10()
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		print_alphabet();
+	}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+int _islower(int c)
+{
+	_putchar('\n');
+	_putchar(c);
+	_putchar('\n');
+	_putchar('\n');
+	return (0);
+}
