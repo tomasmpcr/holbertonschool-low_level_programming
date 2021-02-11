bc746ac82473ef359bef7f1b4fc4fe95701a392b
Tshuna332 <tmmi333@gmail.cm>
2021-02-11 13:18:56 +0000

    auto add

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_line - loco..
+ *@n: number
+ *
+ * Return: void.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
