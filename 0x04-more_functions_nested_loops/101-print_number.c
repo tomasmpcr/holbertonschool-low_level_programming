4955f1934e4073bf11d15a9da7ebca930c602a0b
Tshuna332 <tmmi333@gmail.cm>
2021-02-11 17:46:28 +0000

    auto add

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,18 +8,14 @@
  */
 void print_number(int n)
 {
-	int cal;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	cal =  n / 10;
-
-	if (cal != 0)
-		print_number(cal);
+	if ((n / 10) != 0)
+		print_number(n / 10);
 
 	_putchar((n % 10) + '0');
 }
