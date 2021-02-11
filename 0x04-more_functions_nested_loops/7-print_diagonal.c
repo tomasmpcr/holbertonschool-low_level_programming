2b40d3a0121aa0b1fccb28d711f8ab447c231cb2
Tshuna332 <tmmi333@gmail.cm>
2021-02-11 13:27:02 +0000

    auto add

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - loco..
+ *@n: number
+ *
+ * Return: void.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j <= (i - 1); j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
