b2be9bdfdc46de3a62104e45aebbdf099da44835
Tshuna332 <tmmi333@gmail.cm>
2021-02-11 17:30:04 +0000

    auto add

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,5 @@
 #include "holberton.h"
 
-
-/**
- * print_number2 - loco..
- *@n: num
- *
- * Return: void.
- */
-void print_number2(int n)
-{
-	if (n != 0)
-	{
-		print_number2(n / 10);
-		_putchar((n % 10) + '0');
-	}
-}
-
-
 /**
  * print_number - loco..
  *@n: num
@@ -25,13 +8,25 @@ void print_number2(int n)
  */
 void print_number(int n)
 {
+	int cal;
+
 	if (n == 0)
+	{
 		_putchar('0');
+		return;
+	}
 	else if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	print_number2(n);
+	cal =  n / 10;
+
+	if (cal != 0)
+	{
+		print_number(cal);
+		_putchar((cal % 10) + '0');
+	}
+
 }
---
+++ 0x04-more_functions_nested_loops/prueba.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int pr;
+
+	pr = 0 % 10;
+
+	printf("%d", pr);
+}
