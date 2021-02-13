992940898fe382b359a477d83c8546f769f517d6
Tshuna332 <tmmi333@gmail.cm>
2021-02-13 02:53:32 +0000

    auto add

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * puts2 - loco..
+ *@str: char
+ *
+ * Return: void.
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0';i++)
+	{
+	}
+
+	for (j = 0; j <= i; j = j + 2)
+	{
+		_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}
