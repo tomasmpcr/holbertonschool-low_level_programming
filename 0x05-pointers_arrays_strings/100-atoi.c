b71a32b91fbc344a0c61eeb1e079f4adfc3cd303
tshuna332 <tmmi333@gmail.com>
2021-02-16 16:33:54 +0000

    auto add

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,112 +1,36 @@
 #include "holberton.h"
 
 /**
- * _sumaSignos - loco..
- *@signo: char
- *@nuevo: char
- *
- * Return: char.
- */
-char _sumaSignos(char signo, char nuevo)
-{
-	char ret = '+';
-
-	if (signo == '+')
-	{
-		if (nuevo == '+')
-			ret = '+';
-		else
-			ret = '-';
-	}
-	else
-	{
-		if (nuevo == '+')
-			ret = '-';
-		else
-			ret = '+';
-	}
-
-	return (ret);
-}
-
-
-
-/**
- * _calN - loco..
- *@s: char
- *@j: len
- *@signo: char
- *
- * Return: int.
- */
-int _calN(char *s, int j, char signo)
-{
-	int inc = 1, n = 0;
-
-	for (; j >= 0; j--)
-	{
-		if (s[j] >= '0' && s[j] <= '9')
-		{
-			n = n + ((s[j] - '0') * inc);
-			inc = inc * 10;
-		}
-	}
-
-	if (signo == '-')
-	{
-		if (n > 0)
-		n = -n;
-	}
-
-	return (n);
-}
-
-
-
-
-
-/**
- * _atoi - loco..
+ *_atoi - loco..
  *@s: char
  *
  * Return: int.
  */
 int _atoi(char *s)
 {
-	char signo = '+';
-	int i, j = 0, n = 0, paso = 0, block = 0, primSigno = 0;
+	int n = 0, i, calSig = 0, terminar = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (block == 0)
-		{
-			if (s[i] == '+' || s[i] == '-')
-			{
-				if (primSigno == 0)
-				{
-					primSigno = 1;
-					signo = s[i];
-				}
-				else
-					signo = _sumaSignos(signo, s[i]);
-			}
+		if (s[i] == '+')
+			calSig++;
+		else if (s[i] == '-')
+			calSig--;
 
-			if (s[i] >= '0' && s[i] <= '9')
-				paso = 1;
-			else
-			{
-				if (paso == 1)
-				{
-					j = i - 1;
-					block = 1;
-				}
-			}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			n = (n * 10) + (s[i] - '0');
+			terminar = 1;
 		}
 		else
-			break;
+		{
+			if (terminar == 1)
+				break;
+		}
 	}
-	if (block == 0)
-		j = i;
-	n = _calN(s, j, signo);
+
+	if (calSig < 0)
+		n = -n;
+
 	return (n);
 }
