8dafed174cc70af9a65290b1a3903e1fc53479e4
tshuna332 <tmmi333@gmail.com>
2021-02-17 14:27:46 +0000

    auto add

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,7 +16,7 @@ int _atoi(char *s)
 			calSig *= -1;
 
 		if (s[i] >= '0' && s[i] <= '9')
-			n = n * 10 + calSig * (*(s + i) - '0');
+			n = n * 10 + calSig * (s[i] - '0');
 		else if (n != 0)
 			break;
 	}
