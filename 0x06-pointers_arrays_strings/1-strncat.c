db019a95c6651f9d7207b369e51146a01b31e906
Tshuna332 <tmmi333@gmail.cm>
2021-02-19 14:11:56 +0000

    auto add

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,22 @@
-o
+#include "holberton.h"
+#include "_strlen.c"
+
+/**
+ *_strncat - NSA
+ *@dest: *
+ *@src: *
+ *@n: int
+ *
+ *Return: *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, destC = _strlen(dest);
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[destC + i] = src[i];
+	}
+
+	return dest;
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include "_strlen.c"
+
+/**
+ *_strncpy - NSA
+ *@dest: *
+ *@src: *
+ *@n: int
+ *
+ *Return: *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, srcC = _strlen(src);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < srcC)
+			dest[i] = src[i];
+		else
+		{
+			dest[i++] = '\0';
+		}
+	}
+
+	return dest;
+}
