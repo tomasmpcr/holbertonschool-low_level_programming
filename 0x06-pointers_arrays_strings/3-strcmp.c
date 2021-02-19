1430b8e8c9a95acd1f7d596e28c86343fb1dae0a
Tshuna332 <tmmi333@gmail.cm>
2021-02-19 15:28:34 +0000

    auto add

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include "_strlen.c"
+
+/**
+ *_strcmp - caracol
+ *@s1: *
+ *@s2: *
+ *
+ *Return: *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, s1C = _strlen(s1), s2C = _strlen(s2), con  = 0, ret = 0;
+
+
+	if (s1C > s2C)
+		con = s1C;
+	else
+		con = s2C;
+
+	for (i = 0; i < con; i++)
+	{
+		ret = s1[i] - s2[i];
+
+		if (ret != 0)
+			break;
+	}
+
+	return ret;
+}
