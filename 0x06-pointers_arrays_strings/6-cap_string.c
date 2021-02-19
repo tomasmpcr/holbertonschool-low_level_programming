e974560a52a274e601aea4dd4372a89d01de050c
Tshuna332 <tmmi333@gmail.cm>
2021-02-19 19:46:22 +0000

    auto add

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,17 +8,16 @@
  */
 char *cap_string(char *c)
 {
-	int i, j, l, a;
+	int i, j, l;
 	char car[] = {' ', '\t', 10, 44, 59, 46, '!', '?', '"', '(', ')', '{', '}'};
 
 	l = sizeof(car) / sizeof(char);
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
-		a = c[i - 1];
 		for (j = 0; j < l; j++)
 		{
-			if (a == car[j] && c[i] >= 'a' && c[i] <= 'z')
+			if (c[i - 1] == car[j] && c[i] >= 'a' && c[i] <= 'z')
 			{
 				c[i] -= 32;
 				break;
