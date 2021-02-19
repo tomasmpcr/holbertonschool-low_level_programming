009c974e5c81de27c94fd77b25300156889ee7d5
Tshuna332 <tmmi333@gmail.cm>
2021-02-19 20:01:50 +0000

    auto add

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 
 /**
-* cap_string - convert lowercase first letter from a separator
-* @c: the string given
-* ---------------------------------------
-* Return: new string
-*/
+ *cap_string - 8 (el culo te abrocho)
+ *@c: *
+ *
+ *Return: *
+ */
 char *cap_string(char *c)
 {
-	unsigned int  i, j;
-	char s[] = {32, '\t', '\n', ',', 59, 46, 33, 63, 61, '(', ')', '{', '}'};
+	unsigned int i, j;
+	char car[] = {' ', '\t', 10, 44, 59, 46, '!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
-		for (j = 0; j < sizeof(s); j++)
+		for (j = 0; j < sizeof(car); j++)
 		{
-			if (c[i] == s[j] && c[i + 1] >= 97 && c[i + 1] <= 122)
+			if (c[i] == car[j] && c[i + 1] >= 97 && c[i + 1] <= 122)
 			{
 				c[i + 1] -= 32;
 				break;
