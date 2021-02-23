3dcb249e7fbdbf28d72031add1f1c9439e596cdf
Tshuna332 <tmmi333@gmail.cm>
2021-02-23 23:52:08 +0000

    auto add

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *set_string - change the valuue in pointer
+ *@s: pointer in poiter
+ *@to: pointer
+ *
+ *return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
