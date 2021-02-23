9e976b0becb2a050b2fb2de48bf991129f888238
Tshuna332 <tmmi333@gmail.cm>
2021-02-23 16:50:16 +0000

    auto add

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,9 +10,25 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	int i, j, paso = 0;
 
-	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				paso = 1;
+				break;
+			}
+		}
+		if (!paso)
+		{
+			break;
+		}
 
+		paso = 0;
+	}
 
-	return (s);
+	return (i);
 }
