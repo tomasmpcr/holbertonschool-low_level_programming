1a2e4776098317d4243cce89dba29ed3d1e19cd1
Tshuna332 <tmmi333@gmail.cm>
2021-02-23 18:00:49 +0000

    auto add

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ *_strstr - imp
+ *@haystack: array
+ *@needle: char
+ *
+ *Return: char puntero
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, paso = 0;
+	char *p = &haystack[0];
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] == needle[j])
+				paso = 1;
+			else
+				paso = 0;
+		}
+		if (!paso)
+			p++;
+		else
+			break;
+	}
+
+	if (!paso)
+	{
+		return (0);
+	}
+
+	return (p);
+}
