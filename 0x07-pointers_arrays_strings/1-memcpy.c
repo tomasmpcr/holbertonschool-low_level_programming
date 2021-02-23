32b54fb65651e6439e68955741c32e038f2b7d03
Tshuna332 <tmmi333@gmail.cm>
2021-02-23 13:26:23 +0000

    auto add

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *_memcpy - imp
+ *@s: array
+ *@b: asd
+ *@n: int
+ *
+ *Return: char puntero
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] == '\0')
+			break;
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
