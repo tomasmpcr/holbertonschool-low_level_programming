b16c651b3a0061b1a2a715bfd8e5f5b99925d5e0
Tshuna332 <tmmi333@gmail.cm>
2021-02-24 17:16:39 +0000

    5fixbetty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,18 +5,16 @@
  *@n: int
  *@i: int
  *
- *Return int
+ *Return: int
  */
 int _rec_uno(int n, int i)
 {
 	if ((i * i) == n)
 		return (i);
-	else if((i * i) > n)
+	else if ((i * i) > n)
 		return (-1);
-	else
-	{
-		return (_rec_uno(n, i + 1));
-	}
+
+	return (_rec_uno(n, i + 1));
 }
 
 
@@ -24,13 +22,11 @@ int _rec_uno(int n, int i)
  *_sqrt_recursion - print the nartural square root of a number
  *@n: the int
  *-----------------------------------------------------
- *return: int
+ *Return: int
  */
 int _sqrt_recursion(int n)
 {
-
 	if (n < 0)
 		return (-1);
-	else
-		return (_rec_uno(n, 1));
+	return (_rec_uno(n, 1));
 }
