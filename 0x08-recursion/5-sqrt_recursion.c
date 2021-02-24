89cb9b5294afb9a36c5b67ce4ea0e98bad4d04fb
Tshuna332 <tmmi333@gmail.cm>
2021-02-24 16:53:38 +0000

    5add

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,26 @@
 #include "holberton.h"
 
 /**
+ *_rec_uno - loco
+ *@n: int
+ *@i: int
+ *
+ *Return int
+ */
+int _rec_uno(int n, int i)
+{
+	if ((i * i) == n)
+		return (i);
+	else if((i * i) > n)
+		return (-1);
+	else
+	{
+		return (_rec_uno(n, i + 1));
+	}
+}
+
+
+/**
  *_sqrt_recursion - print the nartural square root of a number
  *@n: the int
  *-----------------------------------------------------
@@ -9,4 +29,8 @@
 int _sqrt_recursion(int n)
 {
 
+	if (n < 0)
+		return (-1);
+	else
+		return (_rec_uno(n, 1));
 }
