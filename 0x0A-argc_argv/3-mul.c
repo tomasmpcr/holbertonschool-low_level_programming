03b7b2d5f337b9eaf88a0401d04c8e605392dd17
Tshuna332 <tmmi333@gmail.cm>
2021-02-26 16:22:37 +0000

    addEj36

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Print the number of the arguments
+ *
+ *return: int 0
+ */
+int main(int argc, char **argv)
+{
+	int mul = 0;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%i\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (mul);
+}
