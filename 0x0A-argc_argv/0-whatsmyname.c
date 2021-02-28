407f775dcf02191948a62612aceebe900af04ca2
Tshuna332 <tmmi333@gmail.cm>
2021-02-28 00:27:25 +0000

    fix_betty_problems

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,15 @@
 
 /**
  *main - function print the argv
+ *@argc: int
+ *@argv: char
  *
  *Return: 0
  */
 int main(int argc, char **argv)
 {
-        while(argc--)
-                printf("%s\n", *argv++);
+	while (argc--)
+		printf("%s\n", *argv++);
+
 	return (0);
 }
