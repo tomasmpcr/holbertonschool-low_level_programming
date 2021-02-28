fc7a2399dfcabf3bf0d4f1b52311f15cac40b5de
Tshuna332 <tmmi333@gmail.cm>
2021-02-28 12:03:29 +0000

    fix_error_program_and_betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,6 +2,46 @@
 #include <stdlib.h>
 
 /**
+ *comMoneda - check the import
+ *@importe: int
+ *
+ *Return: int
+ */
+int comMoneda(int importe)
+{
+	int moneda = 0;
+
+	if (importe >= 25)
+	{
+		importe -= 25;
+		moneda = 25;
+	}
+	else if (importe >= 10)
+	{
+		importe -= 10;
+		moneda = 10;
+	}
+	else if (importe >= 5)
+	{
+		importe -= 5;
+		moneda = 5;
+	}
+	else if (importe >= 2)
+	{
+		importe -= 2;
+		moneda = 2;
+	}
+	else if (importe >= 1)
+	{
+		importe -= 1;
+		moneda = 1;
+	}
+
+	return (moneda);
+}
+
+
+/**
  *main - print the number
  *@argc: int
  *@argv: char
@@ -10,34 +50,21 @@
  */
 int main(int argc, char **argv)
 {
-	int importe = 0, monedas = 0;
+	int importe = 0, monedas = 0, ram = 0;
 
 	if (argc == 2)
 	{
 		importe = atoi(argv[1]);
 		for (; 1 == 1;)
 		{
-			if (importe >= 25)
+			ram = comMoneda(importe);
+
+			if (ram != 0)
 			{
-				importe -= 25;
+				importe -= ram;
+				monedas++;
 			}
-			else if (importe >= 10)
-			{
-				importe -= 10;
-			}
-			else if (importe >= 5)
-			{
-				importe -= 5;
-			}
-			else if (importe >= 2)
-			{
-				importe -= 2;
-			}
-			else if (importe >= 1)
-			{
-				importe -= 1;
-			}
-			monedas++;
+
 			if (importe <= 0)
 				break;
 		}
