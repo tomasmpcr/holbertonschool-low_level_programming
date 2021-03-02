6d1b602e0e231388ec5670ec6592c66e808ad72d
Tshuna332 <tmmi333@gmail.cm>
2021-03-02 20:12:57 +0000

    fix_error_ej3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		buffer[i] = (int *)malloc(sizeof(int) * width);
 		if (buffer[i] == NULL)
 		{
-			for (i = i - i; i >= 0; i--)
+			for (i = i - 1; i >= 0; i--)
 				free(buffer[i]);
 			free(buffer);
 			return (NULL);
