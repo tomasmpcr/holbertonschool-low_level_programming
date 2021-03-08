ec5e60e3ab5c70ae2a092fdc9a60e507a5259072
Tshuna332 <tmmi333@gmail.cm>
2021-03-08 17:14:31 +0000

    auto add

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+int _strlen(char *s);
 
 /**
  *new_dog - create a new dog
@@ -11,14 +12,59 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *my_dog = malloc(sizeof(dog_t));
+	dog_t *my_dog;
+	char *name_c, *owner_c;
+	int i, l_name = _strlen(name), l_owner = _strlen(owner);
 
-	if (my_dog == NULL)
+
+	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	(*my_dog).name = name;
+	name_c = malloc(l_name * sizeof(char));
+	if (name_c == NULL)
+		return (NULL);
+
+	owner_c = malloc(l_owner * sizeof(char));
+	if (owner_c == NULL)
+	{
+		free(name_c);
+		return (NULL);
+	}
+
+	for (i = 0; i <= l_name; i++)
+		name_c[i] = name[i];
+
+	for (i = 0; i <= l_owner; i++)
+		owner_c[i] = owner[i];
+
+	my_dog = malloc(sizeof(dog_t));
+	if (my_dog == NULL)
+	{
+		free(name_c);
+		free(owner_c);
+		return (NULL);
+	}
+
+	(*my_dog).name = name_c;
 	(*my_dog).age = age;
-	(*my_dog).owner = owner;
+	(*my_dog).owner = owner_c;
 
 	return (my_dog);
+}
+
+
+/**
+ * _strlen - lenght the string
+ * @s: string
+ *
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
 }
