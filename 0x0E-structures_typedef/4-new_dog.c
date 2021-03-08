b79266e41be03b0a93fb50f4fdf90128b72d32af
Tshuna332 <tmmi333@gmail.cm>
2021-03-08 16:52:44 +0000

    auto add

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,10 +12,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog = malloc(sizeof(dog_t));
+
 	if (my_dog == NULL)
 		return (NULL);
 
 	(*my_dog).name = name;
+	(*my_dog).age = age;
 	(*my_dog).owner = owner;
 
 	return (my_dog);
