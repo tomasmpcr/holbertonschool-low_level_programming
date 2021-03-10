3671270b9d588f664a1134dc7fb9f4ef7aaf1754
Tshuna332 <tmmi333@gmail.cm>
2021-03-10 15:17:58 +0000

    fix_error

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,24 +9,26 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	if (s != NULL)
-	{
-		op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-		};
-		int i = 0;
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i = 0;
 
-		while (ops[i].op != NULL)
-		{
-			if (ops[i].op[0] == s[0])
-				return (ops[i].f);
-			i++;
-		}
+	if (s == NULL)
+		return (NULL);
+
+
+	while (ops[i].op != NULL)
+	{
+		if (ops[i].op[0] == s[0])
+			return (ops[i].f);
+		i++;
 	}
+
 	return (NULL);
 }
