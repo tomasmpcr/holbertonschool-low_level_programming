26a5f4aff08b4cd21d22c8c9e0ff7000b52ea776
Tshuna332 <tmmi333@gmail.cm>
2021-03-10 15:12:31 +0000

    fix_error

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, ret;
 
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array != NULL ¬ ¬ && cmp != NULL)
 	{
-		ret = cmp(array[i]);
-		if (ret)
-			return (i);
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			ret = cmp(array[i]);
+			if (ret)
+				return (i);
+		}
 	}
 
 	return (-1);
