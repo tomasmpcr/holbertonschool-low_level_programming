5f8b7a896989535eaa26eef0ea750419381b1aea
Tshuna332 <tmmi333@gmail.cm>
2021-03-10 14:57:51 +0000

    auto add

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - the function start
+ *@ac: len in the array
+ *@av: the array of the parametrs
+ *
+ *Return: int
+ */
+int main(int ac, char *av[])
+{
+	int num1, num2, result;
+	char *op = av[2];
+	int (*fun)(int, int);
+
+	if (ac != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num1 = atoi(av[1]);
+	num2 = atoi(av[3]);
+
+	if ((op[0] == '/' || op[0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	fun = get_op_func(op);
+	if (fun == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	result = fun(num1, num2);
+	printf("%i\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ *op_add - function sum
+ *@a: int
+ *@b: int
+ *
+ *Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - function resta
+ *@a: int
+ *@b: int
+ *
+ *Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - function mul
+ *@a: int
+ *@b: int
+ *
+ *Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - function div
+ *@a: int
+ *@b: int
+ *
+ *Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - function mod
+ *@a: int
+ *@b: int
+ *
+ *Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
