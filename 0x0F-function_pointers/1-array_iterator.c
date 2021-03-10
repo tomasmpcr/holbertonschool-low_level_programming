f1a47491ffc4c2b18eda149f6bd859b6be1a1a02
Tshuna332 <tmmi333@gmail.cm>
2021-03-10 15:10:18 +0000

    fix_error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  *array_iterator - print the array
@@ -11,6 +12,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
+
+	if (array == NULL && action == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
