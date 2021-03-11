b51a30f9502818dc7992e5c078f1b7a8ed93c59a
tshuna332 <tmmi333@gmail.com>
2021-03-11 19:18:11 -0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -22,7 +22,7 @@ void print_all(const char * const format, ...)
 	};
 
 	va_start(ap, format);
-	while (format[i] != '\0' && format != NULL)
+	while (format != NULL && format[i] != '\0')
 	{
 		j = 0;
 		while (dt[j].l != NULL)
