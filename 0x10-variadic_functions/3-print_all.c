ea556bffc48aee120ca6a42ad633c0b1bc802c4d
Tshuna332 <tmmi333@gmail.cm>
2021-03-11 13:18:30 +0000

    auto add

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ *print_all - print all the strings
+ *@format: cosas
+ *
+ *Return: void
+ */
+void print_all(const char * const format, ...)
+{
+
+
+
+	printf("%i\n", format);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*	va_list ap;
+	int i, len;
+	char *x;
+
+	if (separator == NULL)
+		return;
+
+	len = n;
+
+	va_start(ap, n);
+	for (i = 0; i < len; i++)
+	{
+		x = va_arg(ap, char*);
+		printf("%s", x);
+		if (i != (len - 1))
+			printf(", ");
+	}
+	va_end(ap);
+	printf("\n");*/
+}
