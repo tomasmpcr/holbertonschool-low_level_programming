4bcd4dec8e1791331af9fd1bac5942b5cd84f86d
Tshuna332 <tmmi333@gmail.cm>
2021-03-11 13:03:24 +0000

    auto add

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ *print_strings - print the strings separeit for the sep
+ *@separator: char *
+ *@n: const unsgned int
+ *
+ *Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int i, len;
+	char *x;
+
+	if (separator == NULL)
+		return;
+
+	len = n;
+
+	va_start(ap, n);
+	for (i = 0; i < len; i++)
+	{
+		x = va_arg(ap, char*);
+		printf("%s", x);
+		if (i != (len - 1))
+			printf(", ");
+	}
+	va_end(ap);
+	printf("\n");
+}
