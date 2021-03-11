6d31950d91cdd7e07635e152f12c5edeacc7f570
Tshuna332 <tmmi333@gmail.cm>
2021-03-11 12:54:44 +0000

    auto add

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ *print_numbers - print the number separeit for the sep
+ *@separator: char *
+ *@n: const unsgned int
+ *
+ *Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int i, x, len;
+
+	if (separator == NULL)
+		return;
+
+	len = n;
+
+	va_start(ap, n);
+	for (i = 0; i < len; i++)
+	{
+		x = va_arg(ap, int);
+		printf("%i", x);
+		if (i != (len - 1))
+			printf(", ");
+	}
+	va_end(ap);
+	printf("\n");
+}
