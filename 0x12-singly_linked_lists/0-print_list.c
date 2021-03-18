ab31986ff292f3f77011fb8437221e3b21ca22d9
tshuna332 <tmmi333gmail.com>
2021-03-18 20:42:29 +0000

    manual add

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,20 +10,17 @@ size_t print_list(const list_t *h)
 {
 	size_t num_node = 0;
 
-	if (h == NULL)
-		return (num_node);
-
-	for(num_node = 0; 1 == 1; num_node++)
+	for (num_node = 0; 1 == 1; num_node++)
 	{
+		if (h == NULL)
+			break;
+
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%i] %s\n", h->len, h->str);
 
-		if (h->next == NULL)
-			break;
-		else
-			h = h->next;
+		h = h->next;
 	}
 
 	return (num_node);
