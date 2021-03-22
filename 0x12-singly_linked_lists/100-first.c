37cc48a396b687054cd6f12f4f4581927164b0d9
tshuna332 <tmmi333gmail.com>
2021-03-22 10:33:29 +0000

    the first ej

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *_start - This is the first function call for the sistem
+ *
+ *Return: void
+ */
+void _start() {
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	int ret = main();
+	exit(ret);
+}
