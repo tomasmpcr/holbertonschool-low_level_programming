8cc49c37e9322a173ee0d8cfca2c1fe3cecf22d3
tshuna332 <tmmi333gmail.com>
2021-03-25 19:10:27 +0000

    add ej1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ *print_binary_r - print binary number
+ *@n: the number unsigned long int
+ *
+ *Return: void
+*/
+void print_binary_r(unsigned long int n)
+{
+	unsigned long int ram;
+
+	if(n == 0)
+		return;
+
+	ram = n & 1;
+
+	n = n >> 1;
+	print_binary_r(n);
+
+	_putchar(ram + '0');
+}
+
+
+/**
+ *print_binary - print binary number
+ *@b: the number unsigned Ãlong int
+ *
+ *Return: void
+*/
+void print_binary(unsigned long int n)
+{
+	if(n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	print_binary_r(n);
+}
