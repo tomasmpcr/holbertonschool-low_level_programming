7c3017906329a8629723ef0476a37efccb440f4b
tshuna332 <tmmi333gmail.com>
2021-03-29 18:10:54 +0000

    add ej1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+*create_file - create file
+*@filename: name of file
+*@text_content: string print
+*
+*Return: letters prints
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	if (fd < 0)
+		return (-1);
+
+	while (text_content != NULL)
+	{
+		if (write(fd, text_content, 1) == -1)
+			return (-1);
+		text_content++;
+	}
+
+	close(fd);
+	return (1);
+}
