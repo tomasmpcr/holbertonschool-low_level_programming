7fc3342c165950851d6f617632008d4f3544ce18
tshuna332 <tmmi333gmail.com>
2021-03-29 18:17:42 +0000

    add holberton.h proto

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	while (text_content != NULL)
+	while(*text_content)
 	{
 		if (write(fd, text_content, 1) == -1)
 			return (-1);
