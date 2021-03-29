7cffb3071cdb84893f0812d76196fb84c9b768e0
tshuna332 <tmmi333gmail.com>
2021-03-29 19:31:32 +0000

    fix error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while(text_content && *text_content)
+	if (text_content == NULL)
+		return (-1);
+
+	while(*text_content)
 	{
 		if ((write(fd, text_content, 1)) == -1)
 		{
@@ -31,3 +34,4 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	return (1);
 }
+
