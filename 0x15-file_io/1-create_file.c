870c0b3e6dabf6cec6afee9be646a4ea8c3886be
tshuna332 <tmmi333gmail.com>
2021-03-30 02:40:02 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,10 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-	{
-		close(fd);
-		return (1);
-	}
+		text_content = "";
 
 	while(*text_content)
 	{
