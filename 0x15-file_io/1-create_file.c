22e17e0382ad100a5d551e86c56c5fd2e279623b
tshuna332 <tmmi333gmail.com>
2021-03-30 02:44:31 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = 0;
+	int fd = 0, i = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -18,17 +18,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
-	while(*text_content)
+	for (i = 0; text_content && text_content[i]; i++)
 	{
-		if (write(fd, text_content, 1) == -1)
+		if (write(fd, &text_content[i], 1) == -1)
 		{
 			close(fd);
 			return (-1);
 		}
-		text_content++;
 	}
 
 	close(fd);
