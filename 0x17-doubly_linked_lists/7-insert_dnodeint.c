1fbc29ac374d394b1d9ccbc76c4578608c990923
mstom332 <tomas@332.com.uy>
2021-05-05 14:00:32 +0000

    fix ej 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,6 +41,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			current->prev = new_node;
 			new_node->next = current;
 
+			if (idx == 0)
+				*h = new_node;
+
 			return (new_node);
 		}
 
