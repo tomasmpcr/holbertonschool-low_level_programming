be66e7947f2f1f6979c873e67c754c6ae8cd0fbb
mstom332 <tomas@332.com.uy>
2021-05-05 13:57:18 +0000

    add ej 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* insert_dnodeint_at_index - insert node in index
+* @h: head nodo
+* @idx: index
+* @n: value n in new node
+* ----------------------------------------------------------------
+* Return: new node
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i;
+	dlistint_t *new_node = NULL;
+	dlistint_t *current = NULL;
+
+	if (h == NULL)
+		return (NULL);
+	current = *h;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	for (i = 0; 1 == 1; i++)
+	{
+		if (i == idx)
+		{
+			if (current->prev != NULL)
+			{
+				new_node->prev = current->prev;
+				current->prev->next = new_node;
+			}
+			current->prev = new_node;
+			new_node->next = current;
+
+			return (new_node);
+		}
+
+		if (current != NULL)
+			current = current->next;
+		else
+			break;
+	}
+
+	return (NULL);
+}
