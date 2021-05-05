c92f1bb3181e12fbdb46b73961e8583f1fa59800
mstom332 <tomas@332.com.uy>
2021-05-05 14:52:44 +0000

    add ej 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* delete_dnodeint_at_index - delete a node in index
+* @head: head nodo
+* @index: index
+* ----------------------------------------------------------------
+* Return: int
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *current = *head, *prev = NULL;
+
+	if (head == NULL)
+		return (-1);
+
+	for (i = 0; 1 == 1; i++)
+	{
+		if (i == index && current)
+		{
+			if (current->next && prev)
+			{
+				prev->next = current->next;
+				current->next->prev = prev;
+			}
+			else if (prev)
+				prev->next = NULL;
+			else
+				*head = current->next;
+
+			free(current);
+			return (1);
+		}
+
+		if (current)
+		{
+			prev = current;
+			current = current->next;
+		}
+		else
+			break;
+	}
+
+	return (-1);
+}
