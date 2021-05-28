d594534df4d0cadfc382ded23ee492128b2893f9
mstom332 <tomas@332.com.uy>
2021-05-28 03:58:24 +0000

    add ej

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,9 +7,11 @@
  *
  * Return: hash value
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    
+    unsigned long int idx;
 
+    idx = hash_djb2(key) % size;
 
+    return (idx);
 }
