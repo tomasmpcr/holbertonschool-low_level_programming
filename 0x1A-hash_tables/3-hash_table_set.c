1ffddbba497a6ccbaf3093cf3ca1cca8968ce8ff
mstom332 <tomas@332.com.uy>
2021-05-28 06:02:15 +0000

    asd

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,13 +14,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *head = NULL;
 	hash_node_t *new_node = NULL;
 
-	if ((ht == NULL || ht->array == NULL) ||
-		(key == NULL || key[0] == '\0') || value == NULL)
+	if ((ht == NULL || ht->array == NULL) || (key == NULL || key[0] == '\0'))
 		return (0);
-
 	idx = key_index((const unsigned char *) key, ht->size);
 	head = ht->array[idx];
-
 	for (; head;)
 	{
 		if (strcmp(head->key, key) == 0)
@@ -33,7 +30,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		head = head->next;
 	}
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
@@ -51,10 +47,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	new_node->next = NULL;
-
 	if (head)
 		new_node->next = head;
-
 	ht->array[idx] = new_node;
 	return (1);
 }
