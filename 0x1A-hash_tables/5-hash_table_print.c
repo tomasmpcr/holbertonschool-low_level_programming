f11a02f14b2905e753cccaf727f3cb83bfb669ca
mstom332 <tomas@332.com.uy>
2021-06-10 14:05:40 +0000

    si

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_print - Function that creates a hash table.
+* @ht: l
+* --------------------------------------------------
+* Return: fin
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	
+}
