4658ae14ae370e2a1b86ae1ce75f24cb54d02c7a
mstom332 <tomas@332.com.uy>
2021-07-02 18:02:46 +0000

    add

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Technical interview preparation
+"""
+
+def island_perimeter(grid):
+    count = 0
+
+    for i in range(1, len(grid) - 1):
+        for j in range(1, len(grid[i]) - 1):
+            if grid[i][j] == 1:
+                if grid[i + 1][j] == 0:
+                    count += 1
+                if grid[i - 1][j] == 0:
+                    count += 1
+                if grid[i][j + 1] == 0:
+                    count += 1
+                if grid[i][j - 1] == 0:
+                    count += 1
+    return(count)
