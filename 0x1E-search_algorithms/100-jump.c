008580182c1e850a4b574c61326841a08084319d
tshuna332 <tmmi333@gmail.com>
2021-10-25 23:22:48 +0000

    asd

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,35 +1,60 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
- * jump_search - ASDASD
- * @array: ASASD
- * @size: ASDASD
- * @value: ASAS
- * Return: ASDASDD
+ * jump_search - ASFDFFDDFDF
+ * @array: ASDFASFAF
+ * @size: ADSFAS
+ * @value: ASFASFDA
+ * Return: -1
  */
-
 int jump_search(int *array, size_t size, int value)
 {
-	size_t gap = sqrt(size), pos = 0;
+	size_t i, i2, salto;
+	int pos;
 
-	if (!array)
-		return (-1);
+	if (array)
+	{
+		salto = sqrt(size);
+		i = 0;
+		i2 = salto;
+		pos = array[i];
+		printf("Value checked array[%lu] = [%d]\n", i, pos);
+		while (i2 < size && pos < value)
+		{
+			if (array[i2] >= value)
+				break;
 
-	printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
-	if (array[0] > value)
-		return (-1);
-	while (pos + gap < size && value > array[pos + gap])
-	{
-		printf("Value checked array[%lu] = [%d]\n", pos + gap, array[pos + gap]);
-		pos += gap;
-	}
-	printf("Value found between indexes [%lu] and [%lu]\n", pos, pos + gap);
-	while (size > pos)
-	{
-		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
-		if (array[pos] == value)
-			return (pos);
-		pos++;
+			i = i + salto;
+			i2 = i2 + salto;
+			pos = array[i];
+			printf("Value checked array[%lu] = [%d]\n", i, pos);
+		}
+
+		if (i >= size || pos > value)
+			return (-1);
+		printf("Value found between indexes [%lu] and [%lu]\n", i, i2);
+		while (i <= test1(size - 1, i2) && pos <= value)
+		{
+			pos = array[i];
+			printf("Value checked array[%lu] = [%d]\n", i, pos);
+			if (pos == value)
+				return (i);
+			i++;
+		}
 	}
 	return (-1);
+}
+
+/**
+ * test1 - ASDASDASD
+ * @primero: ASDASD
+ * @segundo: ASDASD
+ * Return: LA
+ */
+size_t test1(size_t primero, size_t segundo)
+{
+	if (segundo < primero)
+		return (segundo);
+	return (primero);
 }
