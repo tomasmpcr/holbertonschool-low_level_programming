9419efaf34c1972a8085d3dcce47921258af5173
tshuna332 <tmmi333@gmail.com>
2021-10-25 23:42:11 +0000

    asd

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,37 +10,37 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t l = 0, h = size - 1, p;
+    size_t l = 0, h = size - 1, p;
 
-	if (!array)
-		return (-1);
-	while (l <= h)
-	{
-		if (array[l] == array[h] && array[l] == value)
-		{
-		    return (l);
-		}
+    if (!array)
+        return (-1);
+    while (l <= h)
+    {
+        if (array[l] == array[h] && array[l] == value)
+        {
+            return (l);
+        }
         else if (array[l] == array[h])
-		{
-        		return (-1);
-		}
-		p = l +
+        {
+                return (-1);
+        }
+        p = l +
 (((double)(h - l) / (array[h] - array[l])) * (value - array[l]));
-		if (size < p)
-		{
-			printf("Value checked array[%lu] is out of range\n", p);
-			break;
-		}
-		else
-			printf("Value checked array[%lu] = [%d]\n", p, array[p]);
-		if (value == array[p])
-		{
-			return (p);
-		}
-		else if (value < array[p])
-			h = p - 1;
-		else
-			l = p + 1;
-	}
-	return (-1);
+        if (size < p)
+        {
+            printf("Value checked array[%lu] is out of range\n", p);
+            break;
+        }
+        else
+            printf("Value checked array[%lu] = [%d]\n", p, array[p]);
+        if (value == array[p])
+        {
+            return (p);
+        }
+        else if (value < array[p])
+            h = p - 1;
+        else
+            l = p + 1;
+    }
+    return (-1);
 }
